d795aa8ea6c3626ea8fbb8743cfe5dcd7c7b7204
Egor Okhapkin <egor.okhapkin@alibaba-inc.com>
2021-06-14 17:13:40 +0300

    hydra-service setup

--- hydra-client/src/main/kotlin/ru/ohapegor/oidc/hydraclient/config/SecurityConfig.kt
+++ hydra-client/src/main/kotlin/ru/ohapegor/oidc/hydraclient/config/SecurityConfig.kt
@@ -3,22 +3,18 @@ package ru.ohapegor.oidc.hydraclient.config
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
-import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService
-
 
 
 @Configuration
 class SecurityConfig : WebSecurityConfigurerAdapter() {
 
     override fun configure(http: HttpSecurity) {
-        val aliOidUserService = OidcUserService()
-        aliOidUserService.setAccessibleScopes(mutableSetOf("profile","email"))
         http.authorizeRequests { authorizeRequests ->
             authorizeRequests
                     .anyRequest().authenticated()
         }
                 .oauth2Login { oauth2Login ->
-                    oauth2Login.userInfoEndpoint().oidcUserService(aliOidUserService)
+                    //oauth2Login.userInfoEndpoint().oidcUserService(OidcUserService())
                     oauth2Login.redirectionEndpoint().baseUri("/callback")
                 }
     }
